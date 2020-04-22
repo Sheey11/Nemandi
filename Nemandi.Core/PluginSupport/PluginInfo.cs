23ad6025affbb35a33ec987ba2ab0571fe1b7b47
Sheey <1511956322@qq.com>
2020-04-22 10:43:29 +0800

    :construction: replace \t to spaces

--- Nemandi.Core/PluginSupport/PluginInfo.cs
+++ Nemandi.Core/PluginSupport/PluginInfo.cs
@@ -6,24 +6,24 @@ using Nemandi.PluginBase.Configurations;
 using Nemandi.Infrastructure;
 
 namespace Nemandi.Core.PluginSupport {
-	public class PluginInfo {
+    public class PluginInfo {
 
-		public IPlugin Instance { get; set; }
+        public IPlugin Instance { get; set; }
 
         public string FilePath { get; set; }
-		public string Name => Instance.Name;
-		public string Author => Instance.Author;
-		public string Website => Instance.Website;
-		public Version Version => Instance.Version;
-		public string Email => Instance.Email;
-		public string Description => Instance.Description;
+        public string Name => Instance.Name;
+        public string Author => Instance.Author;
+        public string Website => Instance.Website;
+        public Version Version => Instance.Version;
+        public string Email => Instance.Email;
+        public string Description => Instance.Description;
 
-		public Languages SourceLang => Instance.SourceLang;
-		public Languages QueryLang => Instance.QueryLang;
-		public Features SupportedFeature => Instance.SupportedFeature;
+        public Languages SourceLang => Instance.SourceLang;
+        public Languages QueryLang => Instance.QueryLang;
+        public Features SupportedFeature => Instance.SupportedFeature;
 
-		public bool Enabled { get; set; }
+        public bool Enabled { get; set; }
 
-		public PluginInfo() { }
-	}
+        public PluginInfo() { }
+    }
 }
--- Nemandi.Core/UserSettings/Preferences.cs
+++ Nemandi.Core/UserSettings/Preferences.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Globalization;
+
 namespace Nemandi.Core.UserSettings {
     public static class Preferences {
+        public static CultureInfo UILanguage => CultureInfo.CurrentUICulture;
         // TODO
     }
 }
