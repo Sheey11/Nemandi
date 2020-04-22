1e575f9dc6a4b1fdcfcfd930de6b2b5a74a34ded
Sheey <1511956322@qq.com>
2020-04-22 11:21:53 +0800

    :package: chore: add .travis.yml

--- src/Nemandi.PluginBase/Api.cs
+++ src/Nemandi.PluginBase/Api.cs
@@ -4,8 +4,9 @@ using Nemandi.Infrastructure;
 
 namespace Nemandi.PluginBase {
     public static class Api {
-        public CultureInfo GetCurrentCulture() {
+        public static CultureInfo GetCurrentCulture() {
             // TODO
+            throw new NotImplementedException();
         }
     }
 }
