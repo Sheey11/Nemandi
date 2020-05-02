2e335c3587c0fde63e23a36398130dfd5a4d76bc
Sheey <1511956322@qq.com>
2020-05-02 11:49:20 +0800

    :sparkles: add prference saver

--- src/Nemandi.Core/UserSettings/Preferences.cs
+++ src/Nemandi.Core/UserSettings/Preferences.cs
@@ -1,9 +1,40 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Reflection;
 using System.Globalization;
 
 namespace Nemandi.Core.UserSettings {
     public static class Preferences {
         public static CultureInfo UILanguage => CultureInfo.CurrentUICulture;
         // TODO
+
+        private static string PreferenceFilePath => Path.GetFileName(@"./config/preference.json");
+        private static string PreferenceFolder => Path.GetDirectoryName(PreferenceFilePath);
+
+        public static void Save() {
+            Directory.CreateDirectory(PreferenceFolder);
+
+            using var stream = File.OpenWrite(PreferenceFilePath);
+            using var writer = new Utf8JsonWriter(stream);
+            writer.WriteStartObject();
+
+            foreach (var item in typeof(Preferences).GetProperties(BindingFlags.Public)) {
+                var value = item.GetConstantValue();
+                switch (value) {
+                    case bool b:
+                        writer.WriteBoolean(item.Name, b);
+                        break;
+                    case int i:
+                        writer.WriteNumber(item.Name, i);
+                        break;
+                    default:
+                        writer.WriteString(item.Name, value.ToString());
+                        break;
+                }
+            }
+            writer.WriteEndObject();
+            writer.Flush();
+        }
     }
 }
