559ed73286acaad06d1263cfbc204d9db7b9a6a2
Sheey <1511956322@qq.com>
2020-05-19 15:23:44 +0800

    :sparkles: added support for password config type

--- src/Nemandi.Core/UserSettings/PluginSettings.cs
+++ src/Nemandi.Core/UserSettings/PluginSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
 using Nemandi.PluginBase;
@@ -24,33 +25,36 @@ namespace Nemandi.Core.UserSettings {
         public void Save() {
             Directory.CreateDirectory(this.ConfigFileFolder);
 
-            using (var fs = File.OpenWrite(this.ConfigFilePath)) {
-                using(var writer = new Utf8JsonWriter(fs)) {
-                    // start of the json
-                    writer.WriteStartObject();
-                    foreach(var item in this.Configurations) {
-                        // start of the item
-                        writer.WriteStartObject(item.Name);
-                        // item configuration
-                        switch (item) {
-                            case BoolConfigItem c when item.Type == ConfigurationTypes.Bool:
-                                writer.WriteBoolean("Value", c.Value);
-                                break;
-                            case DecimalConfigItem c when item.Type == ConfigurationTypes.Decimal:
-                                writer.WriteNumber("Value", c.Value);
-                                break;
-                            case IntengerConfigItem c when item.Type == ConfigurationTypes.Intenger:
-                                writer.WriteNumber("Value", c.Value);
-                                break;
-                            case TextConfigItem c when item.Type == ConfigurationTypes.Text:
-                                writer.WriteString("Value", c.Value);
-                                break;
-                        }
-                        writer.WriteEndObject();
-                    }
-                    writer.WriteEndObject();
+            using var fs = File.OpenWrite(this.ConfigFilePath);
+            using var writer = new Utf8JsonWriter(fs);
+            // start of the json
+            writer.WriteStartObject();
+            foreach (var item in this.Configurations) {
+                // start of the item
+                writer.WriteStartObject(item.Name);
+                // item configuration
+                switch (item) {
+                    case BoolConfigItem c when item.Type == ConfigurationTypes.Bool:
+                        writer.WriteBoolean("Value", c.Value);
+                        break;
+                    case DecimalConfigItem c when item.Type == ConfigurationTypes.Decimal:
+                        writer.WriteNumber("Value", c.Value);
+                        break;
+                    case IntengerConfigItem c when item.Type == ConfigurationTypes.Intenger:
+                        writer.WriteNumber("Value", c.Value);
+                        break;
+                    case TextConfigItem c when item.Type == ConfigurationTypes.Text:
+                        writer.WriteString("Value", c.Value);
+                        break;
+                    case PasswordConfigItem c when item.Type == ConfigurationTypes.Password:
+                        var bytes = Encoding.UTF8.GetBytes(c.Value);
+                        var bs64 = Convert.ToBase64String(bytes);
+                        writer.WriteString("Value", bs64);
+                        break;
                 }
+                writer.WriteEndObject();
             }
+            writer.WriteEndObject();
         }
 
         public Task SaveAsync() {
@@ -61,36 +65,42 @@ namespace Nemandi.Core.UserSettings {
             if (!File.Exists(this.ConfigFilePath))
                 return;
 
-            using (var fs = File.OpenRead(this.ConfigFilePath)) {
-                using(var document = JsonDocument.Parse(fs)) {
-                    var root = document.RootElement;
+            using var fs = File.OpenRead(this.ConfigFilePath);
+            using var document = JsonDocument.Parse(fs);
+            var root = document.RootElement;
 
-                    foreach (var item in this.Configurations) {
-                        // compatible with new version of plugin which has more config items
-                        JsonElement ele;
-                        try {
-                            ele = root.GetProperty(item.Name);
-                        } catch(KeyNotFoundException) {
-                            continue;
-                        } catch (Exception e) {
-                            throw e;
-                        }
+            foreach (var item in this.Configurations) {
+                // compatible with new version of plugin which has more config items
+                JsonElement ele;
+                try {
+                    ele = root.GetProperty(item.Name);
+                }
+                catch (KeyNotFoundException) {
+                    continue;
+                }
+                catch (Exception e) {
+                    throw e;
+                }
 
-                        switch (item.Type) {
-                            case ConfigurationTypes.Bool:
-                                item.SetInitValue(ele.GetProperty("Value").GetBoolean());
-                                break;
-                            case ConfigurationTypes.Decimal:
-                                item.SetInitValue(ele.GetProperty("Value").GetDouble());
-                                break;
-                            case ConfigurationTypes.Intenger:
-                                item.SetInitValue(ele.GetProperty("Value").GetInt32());
-                                break;
-                            case ConfigurationTypes.Text:
-                                item.SetInitValue(ele.GetProperty("Value").GetString());
-                                break;
-                        }
-                    }
+                switch (item.Type) {
+                    case ConfigurationTypes.Bool:
+                        item.SetInitValue(ele.GetProperty("Value").GetBoolean());
+                        break;
+                    case ConfigurationTypes.Decimal:
+                        item.SetInitValue(ele.GetProperty("Value").GetDouble());
+                        break;
+                    case ConfigurationTypes.Intenger:
+                        item.SetInitValue(ele.GetProperty("Value").GetInt32());
+                        break;
+                    case ConfigurationTypes.Text:
+                        item.SetInitValue(ele.GetProperty("Value").GetString());
+                        break;
+                    case ConfigurationTypes.Password:
+                        var bs64 = ele.GetProperty("Value").GetString();
+                        var bytes = Convert.FromBase64String(bs64);
+                        var str = Encoding.UTF8.GetString(bytes);
+                        item.SetInitValue(str);
+                        break;
                 }
             }
         }
