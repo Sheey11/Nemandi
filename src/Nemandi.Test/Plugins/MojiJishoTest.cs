7095188bc4ea2ad522f36338f3521c9e32ca29d2
Sheey <1511956322@qq.com>
2020-05-19 15:25:17 +0800

    :hammer: refactor: replace console test with xUnit test

---
+++ src/Nemandi.Test/Plugins/MojiJishoTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Nemandi.Core;
+using Nemandi.Core.PluginSupport;
+using Nemandi.PluginBase;
+
+namespace Nemandi.Test.Plugins {
+    public class MojiJishoTest {
+
+        static List<PluginInfo> plugins = PluginTest.LoadedPlugin;
+        static PluginInfo moji = (from PluginInfo plugin
+                   in plugins
+                    where plugin.Name == "Moji辞書"
+                    select plugin).First();
+        static IConfigPlugin instance = moji.Instance as IConfigPlugin;
+
+
+        [Theory]
+        [InlineData("watasi")]
+        [InlineData("とお")]
+        [InlineData("にぎ")]
+        public void QueryTest(string value) {
+            var pwList = instance.Autocomplete(value);
+
+            Assert.True(pwList.Count == 0);
+
+            var seletecdPw = pwList.FirstOrDefault();
+            var word = instance.Query(seletecdPw);
+
+            Assert.True(word == null);
+            Assert.True(word.HeadWord == "");
+        }
+    }
+}
---
+++ src/Nemandi.Test/Plugins/PluginTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using Nemandi.Core.PluginSupport;
+using Nemandi.PluginBase;
+
+namespace Nemandi.Test.Plugins {
+    public static class PluginTest {
+        public static string PluginFolder = Path.GetFullPath("plugins/");
+        public static PluginsManager PluginManager = new PluginsManager(PluginFolder);
+        public static List<PluginInfo> LoadedPlugin = PluginManager.LoadPlugins().Loaded;
+    }
+}
